62019aeb8e3c39e2c5f3176d1e3b0e2f0d28df31
Lefie <qiwenruan11@gmail.com>
2024-09-09 17:11:54 -0400

    add the 'add expense' feature

--- expense-tracker.py
+++ expense-tracker.py
@@ -1,66 +1,6 @@
 #!/usr/bin/env python
 import argparse
-import json
-import os
-
-# functions 
-def add_description(description) :
-    print("the description", description)
-
-def add_amount(amount):
-    print("the amount added : ", int(amount))
-
-def listing_expenses(args=None):
-    print("listing item1 ")
-    print("listing item1 ")
-
-def summary_expense(arg=None):
-   print("some expenses")
-       
-def summary_expense_by_month(args):
-    if args.month == 0:
-        print("there is no month input")
-        print(args)
-    else:
-        print(args)
-        print("this month is", args.month)
-
-def update(args):
-    print("update")
-    print(args)
-
-def delete(args):
-    print("delete")
-    print(args)
-
-
-def write_object_to_json(fn,data):
-    # check if file exists
-    # if file does not exist
-    file_path = '../expense-tracker/' + fn
-
-    if os.path.isfile(file_path) == False or os.stat(fn).st_size == 0 :  # if file does not already exist, or file is empty 
-        content = []
-        content.append(data)
-        with open(fn,'w') as outfile:
-            json.dump(content,outfile)
-    else: # file does exist
-        file = open(fn)
-        content = json.load(file)
-        content.append(data)
-        with open(fn, 'w') as outfile:
-            json.dump(content,outfile)
-
-
-def read_from_json(fn):
-    file_path = '../expense-tracker/' + fn
-    if os.path.isfile(file_path) == True:
-        file = open(fn)
-        data = json.load(file)
-        return data
-    else:
-        return fn + " does not exist"
-
+import helper_functions
 
 
 # main function
@@ -76,30 +16,31 @@ def main(command_line=None):
 
     # subparser: add
     add_parser = subparsers.add_parser('add', help='add an expense to track')
-    add_parser.add_argument('--description', help='add the expense description', type=add_description, required=True)
-    add_parser.add_argument('--amount', help='add amount', type=add_amount, required=True)
+    add_parser.add_argument('--description', help='add the expense description', type=str, required=True)
+    add_parser.add_argument('--amount', help='add amount', type=float, required=True)
+    add_parser.set_defaults(func=helper_functions.adding_expense)
 
     # subparser: listing - view
     list_parser = subparsers.add_parser('list', help='listing all the expenses recorded')
-    list_parser.set_defaults(func=listing_expenses)
+    list_parser.set_defaults(func=helper_functions.listing_expenses)
 
     # subparser : summary
     summary_parser = subparsers.add_parser('summary', help = "give a summary of all the expenses ")
     # nargs='?' means 0 or 1 argument
     summary_parser.add_argument('--month', nargs='?', const = 0, type=int)
-    summary_parser.set_defaults(func=summary_expense_by_month)
+    summary_parser.set_defaults(func=helper_functions.summary_expense_by_month)
 
     # subparser: update 
     update_parser = subparsers.add_parser('update', help="update the expenses")
     update_parser.add_argument("--id",type=int, required=True)
     update_parser.add_argument("--description", type=str )
     update_parser.add_argument("--amount",type=float)
-    update_parser.set_defaults(func=update)
+    update_parser.set_defaults(func=helper_functions.update)
 
     #subparser: delete 
     delete_parser = subparsers.add_parser("delete", help="delete an expense")
     delete_parser.add_argument("--id", type=int, required=True)
-    delete_parser.set_defaults(func=delete)
+    delete_parser.set_defaults(func=helper_functions.delete)
 
     args = parser.parse_args(command_line)
     # print("*****")
@@ -108,6 +49,7 @@ def main(command_line=None):
 
     if hasattr(args, 'func'):
         args.func(args)
+
 
 
 
---
+++ helper_functions.py
@@ -0,0 +1,91 @@
+
+import json
+import os
+from datetime import date
+
+# functions 
+def adding_expense(args=None):
+    desc = args.description
+    amount = args.amount
+    current_data = read_from_json('data.json')
+    
+    # construct the data object
+    id = len(current_data) + 1
+    
+    day = f"{date.today().year}/{date.today().month}/{date.today().day}"
+
+    data_obj = {
+        "id":id,
+        "date":day,
+        "description":desc,
+        "amount":amount
+    }
+
+    write_object_to_json('data.json',data_obj )
+    print("adding an expense",desc,amount)
+
+def listing_expenses(args=None):
+    print("listing item1 ")
+    print("listing item1 ")
+
+def summary_expense(arg=None):
+   print("some expenses")
+       
+def summary_expense_by_month(args):
+    if args.month == 0:
+        print("there is no month input")
+        print(args)
+    else:
+        print(args)
+        print("this month is", args.month)
+
+def update(args):
+    print("update")
+    print(args)
+
+def delete(args):
+    print("delete")
+    print(args)
+
+
+def write_object_to_json(fn,data):
+    # check if file exists
+    # if file does not exist
+    file_path = '../expense-tracker/' + fn
+
+    if os.path.isfile(file_path) == False or os.stat(fn).st_size == 0 :  # if file does not already exist, or file is empty 
+        content = []
+        content.append(data)
+        with open(fn,'w') as outfile:
+            json.dump(content,outfile)
+    else: # file does exist
+        file = open(fn)
+        content = json.load(file)
+        content.append(data)
+        with open(fn, 'w') as outfile:
+            json.dump(content,outfile)
+
+
+def read_from_json(fn):
+    file_path = '../expense-tracker/' + fn
+    if os.path.isfile(file_path) == True:
+        file = open(fn)
+        data = json.load(file)
+        return data
+    else:
+        with open(fn, 'w') as outfile:
+            json.dump([],outfile)
+        file = open(fn)
+        data = json.load(file)
+        return data
+
+
+"""
+data structure 
+{
+"id"
+"date"
+"description"
+"amount"
+}
+"""
